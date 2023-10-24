d98b0cfd94ce99a9af3f9f66a3c9e5485797d364
Louisa Shi <louisa_shi@126.com>
2023-10-23 17:35:23 -0700

    update floodfill

--- Flood_Fill.py
+++ Flood_Fill.py
@@ -26,7 +26,10 @@ def flood_fill(input_board: List[str], old: str, new: str, x: int, y: int) -> Li
     """
 
     # Implement your code here.
-    if x < 0 or x >= len(input_board) or y < 0 or y >= len(input_board[0]) or input_board[x][y] != old:
+    x_len = len(input_board)
+    y_len = len(input_board[0])
+    
+    if x < 0 or x >=  x_len or y < 0 or y >= y_len or input_board[x][y] != old:
         return input_board
     else:
         input_board[x] = input_board[x][:y] + new + input_board[x][y+1:]
@@ -34,7 +37,6 @@ def flood_fill(input_board: List[str], old: str, new: str, x: int, y: int) -> Li
         input_board = flood_fill(input_board, old, new, x-1, y)
         input_board = flood_fill(input_board, old, new, x, y+1)
         input_board = flood_fill(input_board, old, new, x, y-1)
-
         return input_board
 
 
